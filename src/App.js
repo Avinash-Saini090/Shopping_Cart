7708549796e447225af2b11dfeef73bd8807b767
Half mind <avisaini909@gmail.com>
2023-08-11 15:56:03 +0530

    added pages and installed reactstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import { BrowserRouter } from "react-router-dom";
+import Category from "./pages/Category";
+import Checkout from "./pages/Checkout";
 
 
 function App() {
   return (
     <div>
-      hello
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Category" element={ <Category /> } />
+      <Route path="/Checkout" element={ <Checkout /> } />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
