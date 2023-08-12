e84d9564e9b950c3fec95c0db74aa4a619ed7cd7
Half mind <avisaini909@gmail.com>
2023-08-12 15:44:37 +0530

    added files and new dependencies

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import { Route, Routes } from "react-router";
-import Home from "./pages/Home";
-import { BrowserRouter } from "react-router-dom";
-import Category from "./pages/Category";
-import Checkout from "./pages/Checkout";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Container } from 'reactstrap';
+import Navbar from './components/Navbar';
+import { CartProvider } from './lib/cart.context';
+import Category from './pages/Category';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Category" element={ <Category /> } />
-      <Route path="/Checkout" element={ <Checkout /> } />
-      </Routes>
-      </BrowserRouter>
-    </div>
+    <CartProvider>
+      <Container>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/category/:id" exact>
+            <Category />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout />
+          </Route>
+        </Switch>
+      </Container>
+    </CartProvider>
   );
 }
 
